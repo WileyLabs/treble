f87d6307c5503a932517007569d0a13e7fc940c6
BigBlueHat <byoung@bigbluehat.com>
2017-03-08 14:48:16 -0500

    Simplify executeScript code in popup.js

--- extension/popup.js
+++ extension/popup.js
@@ -2,13 +2,10 @@
 $('.tabular.menu .item').tab();
 
 // WebExtension cross-border communication
-chrome.tabs.query({active: true, currentWindow: true},
-  function(tabs) {
-    // add the downloader to the current page
-    chrome.tabs.executeScript(tabs[0].id, {
-      file: 'lib/collect.js'
-    });
-  });
+// add the downloader to the current page
+chrome.tabs.executeScript(null, {
+  file: 'lib/collect.js'
+});
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   if ('turtle' in request) {
